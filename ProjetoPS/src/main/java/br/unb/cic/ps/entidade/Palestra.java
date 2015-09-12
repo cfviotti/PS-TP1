8d7ddd288c8292eea5b218a4487234328359b168
Leandro Bergmann <leandro.bergmann@hotmail.com>
2015-09-11 22:55:23 -0300

    Preencher atributos basicos das entidades (temporario)

--- ProjetoPS/src/main/java/br/unb/cic/ps/entidade/Palestra.java
+++ ProjetoPS/src/main/java/br/unb/cic/ps/entidade/Palestra.java
@@ -2,4 +2,54 @@ package br.unb.cic.ps.entidade;
 
 public class Palestra {
 
+	private String nome;
+	
+	private Palestrante palestrante;
+	
+	private String tema;
+	
+	private String local;
+	
+	private String duracao;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Palestrante getPalestrante() {
+		return palestrante;
+	}
+
+	public void setPalestrante(Palestrante palestrante) {
+		this.palestrante = palestrante;
+	}
+
+	public String getTema() {
+		return tema;
+	}
+
+	public void setTema(String tema) {
+		this.tema = tema;
+	}
+
+	public String getLocal() {
+		return local;
+	}
+
+	public void setLocal(String local) {
+		this.local = local;
+	}
+
+	public String getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(String duracao) {
+		this.duracao = duracao;
+	}
+	
 }
--- ProjetoPS/src/main/java/br/unb/cic/ps/entidade/Palestrante.java
+++ ProjetoPS/src/main/java/br/unb/cic/ps/entidade/Palestrante.java
@@ -2,4 +2,24 @@ package br.unb.cic.ps.entidade;
 
 public class Palestrante {
 
+	private String nome;
+	
+	private String disponibilidade;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDisponibilidade() {
+		return disponibilidade;
+	}
+
+	public void setDisponibilidade(String disponibilidade) {
+		this.disponibilidade = disponibilidade;
+	}
+	
 }
