712e4c1deeedbc9bf891d3cec36c133a4d7315b3
Leandro Bergmann <leandro.bergmann@hotmail.com>
2015-09-18 23:15:07 -0300

    Refatorar modulo controle

--- ProjetoPS/src/main/java/br/unb/cic/ps/controle/Controle.java
+++ ProjetoPS/src/main/java/br/unb/cic/ps/controle/Controle.java
@@ -1,7 +1,18 @@
 package br.unb.cic.ps.controle;
 
+import java.util.List;
+import java.util.Map;
+
+import br.unb.cic.ps.entidade.Palestra;
+
 public interface Controle {
 
 	boolean controleTest();
 	
+	void alocarPalestras(List<Palestra> palestras);
+	
+	void removerPalestrasSemHorario(List<Palestra> palestras);
+	
+	Map<Integer, List<Palestra>> gerarMapaPalestras(List<Palestra> palestras);
+
 }
