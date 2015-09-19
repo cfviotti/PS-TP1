1e7a41ddb56eacea36dcbc8c0ce6ab6f5f209d4d
Leandro Bergmann <leandro.bergmann@hotmail.com>
2015-09-18 23:16:29 -0300

    Adicionar datas de inicio e fim das palestras

--- ProjetoPS/src/main/java/br/unb/cic/ps/entidade/Palestra.java
+++ ProjetoPS/src/main/java/br/unb/cic/ps/entidade/Palestra.java
@@ -1,6 +1,8 @@
 package br.unb.cic.ps.entidade;
 
-public class Palestra {
+import java.util.Calendar;
+
+public class Palestra implements Comparable<Palestra> {
 
 	private String nome;
 	
@@ -11,11 +13,29 @@ public class Palestra {
 	private String local;
 	
 	private Integer duracao;
+	
+	private Calendar dataInicio;
+	
+	private Calendar dataFim;
 
 	@Override
 	public String toString() {
-		return "Nome: " + nome + ", Nome do Palestrante: " + palestrante.getNome() +
-				", Tema: " + tema +	", Local: " + local + ", Duracao: " + duracao + " minutos";
+		return "Nome: " + nome +
+				", Nome do Palestrante: " + palestrante.getNome() +
+				", Tema: " + tema +
+				", Local: " + local +
+				", Duracao: " + duracao + " minutos" +
+				", Data: "  + (dataInicio != null ? dataInicio.getTime() : "Sem horario.");
+	}
+	
+	@Override
+	public int compareTo(Palestra palestra) {
+		if (dataInicio.before(palestra.getDataInicio())) {
+			return -1;
+		} else if (dataInicio.after(palestra.getDataInicio())) {
+			return 1;
+		}
+		return 0;
 	}
 	
 	public String getNome() {
@@ -57,5 +77,21 @@ public class Palestra {
 	public void setDuracao(Integer duracao) {
 		this.duracao = duracao;
 	}
-	
+
+	public Calendar getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(Calendar dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public Calendar getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(Calendar dataFim) {
+		this.dataFim = dataFim;
+	}
+
 }
