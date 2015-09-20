44eb08f2ead661bf44658a7e44b9066c4ed0ef9d
ian <ianmalm.ufrj@gmail.com>
2015-09-20 11:46:10 -0300

    Adicionando testes unitários em CalendarioTest e TratamentoTest

--- ProjetoPS/src/test/java/br/unb/cic/ps/calendario/CalendarioTest.java
+++ ProjetoPS/src/test/java/br/unb/cic/ps/calendario/CalendarioTest.java
@@ -1,16 +1,38 @@
 package br.unb.cic.ps.calendario;
 
+import java.util.Calendar;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import junit.framework.TestCase;
 
 public class CalendarioTest extends TestCase {
+	
+	private final static int DOMINGO = 1;
+	private final static int SEGUNDA = 2;
+	private final static int TERCA   = 3;
+	private final static int QUARTA  = 4;
+	private final static int QUINTA  = 5;
+	private final static int SEXTA   = 6;
+	private final static int SABADO  = 7;
 
-	Calendario moduloCalendario = new CalendarioImpl();
+	private Calendario moduloCalendario = new CalendarioImpl();
 	
 	@Test
 	public void testBuscarDiaSemana() {
-		assertTrue(true);
+		assertTrue(moduloCalendario.buscarDiaSemana("Dom")==DOMINGO);
+		assertTrue(moduloCalendario.buscarDiaSemana("Seg")==SEGUNDA);
+		assertTrue(moduloCalendario.buscarDiaSemana("Ter")==TERCA  );
+		assertTrue(moduloCalendario.buscarDiaSemana("Qua")==QUARTA );
+		assertTrue(moduloCalendario.buscarDiaSemana("Qui")==QUINTA );
+		assertTrue(moduloCalendario.buscarDiaSemana("Sex")==SEXTA  );
+		assertTrue(moduloCalendario.buscarDiaSemana("Sab")==SABADO );
+	}
+	
+	@Before
+	public void inicializando() {
+		Calendar calendarTest = Calendar.getInstance();
 	}
 	
 	@Test
--- ProjetoPS/src/test/java/br/unb/cic/ps/tratamento/TratamentoTest.java
+++ ProjetoPS/src/test/java/br/unb/cic/ps/tratamento/TratamentoTest.java
@@ -1,21 +1,53 @@
 package br.unb.cic.ps.tratamento;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import br.unb.cic.ps.leitura.Leitura;
+import br.unb.cic.ps.leitura.LeituraImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import junit.framework.TestCase;
 
 public class TratamentoTest extends TestCase {
 
 	Tratamento moduloTratamento = new TratamentoImpl();
+	Leitura moduloLeitura = new LeituraImpl();
+	
+	List<String[]> dadosPalestras = new ArrayList<>();
+	List<String[]> dadosPalestrantes = new ArrayList<>();
+
+	@Before
+	public void inicializando() {
+		dadosPalestras = moduloLeitura.lerArquivoPalestras("PalestrasTeste.txt");
+		dadosPalestrantes = moduloLeitura.lerArquivoPalestrantes("PalestrantesTeste.txt");
+	}
 	
 	@Test
 	public void testTratarDadosPalestras() {
-		assertTrue(true);
+		inicializando();
+		assertTrue((moduloTratamento.tratarDadosPalestras(dadosPalestras)).toString().contains("Nome: NomePalestra1, " +
+				                                                                       "Nome do Palestrante: Palestrante1, " +
+				                                                                       "Tema: TemaPalestra1, " +
+				                                                                       "Local: Local1, " +
+				                                                                       "Duracao: 90 minutos, " +
+				                                                                       "Data: Sem horario."));	
 	}
 	
 	@Test
 	public void testTratarDadosPalestrantes() {
-		assertTrue(true);
+		inicializando();
+		assertTrue((moduloTratamento.tratarDadosPalestrantes(dadosPalestrantes)).toString().contains("Nome: Palestrante1; " +
+				                                                                 "Disponibilidade: Dia da semana: 3; " +
+				                                                                 "Data de Inicio: Thu Jan 01 10:00:00 BRST 2015; " +
+				                                                                 "Data de Fim: Thu Jan 01 12:00:00 BRST 2015; " +
+				                                                                 "Disponivel toda semana: false; " +
+				                                                                 "Dia da semana: 4; " +
+				                                                                 "Data de Inicio: Wed Dec 31 08:00:00 BRST 2014; " +
+				                                                                 "Data de Fim: Wed Dec 31 09:30:00 BRST 2014; " +
+				                                                                 "Disponivel toda semana: true"));
 	}
 
 	@Test
